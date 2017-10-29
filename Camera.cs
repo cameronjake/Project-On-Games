04f388390bd68a6ab92f1dba4108b4b448488600
cameronjake <camerongeuzinge@hotmail.com>
2017-10-30 00:17:32 +0100

    refresh

---
+++ Camera.cs
@@ -0,0 +1,20 @@
+﻿using GXPEngine;
+
+
+public class Camera : Sprite{
+//    private float x;
+//    private float y;
+    private GameObject hGameObject;
+    public Camera(GameObject hGameObject) : base("transparent.png"){
+        this.hGameObject = hGameObject;
+        SetOrigin(32,32);
+        x = hGameObject.x;
+        y = hGameObject.y;
+    }
+
+    void Update(){
+        x = hGameObject.x;
+        y = hGameObject.y;
+    }
+    
+}

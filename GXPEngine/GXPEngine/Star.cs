beb95f76ae0ebd041e034920ae8e19f86624ad47
cameronjake <camerongeuzinge@hotmail.com>
2017-10-31 00:42:50 +0100

    changed the stars

--- GXPEngine/GXPEngine/Star.cs
+++ GXPEngine/GXPEngine/Star.cs
@@ -6,7 +6,7 @@ namespace GXPEngine{
             Random rnd = new Random(Guid.NewGuid().GetHashCode());
             x = rnd.Next(0,1920);
             y = rnd.Next(0,600);
-            SetScaleXY(rnd.Next(1, 3));
+            SetScaleXY(rnd.Next(1, 4));
             
         }
     }
